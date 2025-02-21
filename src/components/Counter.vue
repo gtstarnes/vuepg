c1509057d9e2c9bd30994a9535b8f6e5a6243b36
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-21 11:05:01 -0500

    create add function

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -4,16 +4,16 @@
     const count = ref<number>(0);
     const square = computed(() => {
         return count.value * count.value
-    })
+    }, [count.value])
 </script>
 
 <template>
     <section>
         <div> {{ count }}</div>
         <div>
-            <button></button>
-            <button></button>
-            <button></button>
+            <button @click="count++">+</button>
+            <button>clr</button>
+            <button>-</button>
         </div>
         <div>{{ square }}</div>
     </section>
