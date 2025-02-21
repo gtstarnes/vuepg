dee5b5791b096de4d29fdc16caee03eeeaf8656a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-21 11:01:55 -0500

    import Counter Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue'
 </script>
 
 <template>
   <HelloWorld />
+  <Counter />
 </template>
 
 <style scoped>
