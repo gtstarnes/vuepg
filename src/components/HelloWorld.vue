106f802d4649d68684f5a30c7bbc26d06745440e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-24 14:58:44 -0500

    reset

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 
-
-  const name = ref('')
 </script>
 
 <template>
-  <input type="text" placeholder="enter a name" v-model="name" />  
-  <h1 v-if="name !== ''">Hello, {{ name }}</h1>
-  <h1 v-else>waiting for you to introduce yourself</h1>
+
 </template>
 
 <style scoped>
