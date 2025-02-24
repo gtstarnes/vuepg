b50763fd0a57e4ea7af82b2f2c788bb29874a402
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-24 14:56:40 -0500

    create variables

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 
+    const count = ref(0)
+    const square = computed(()=> {
+        return count.value * count.value
+    })
 </script>
 
 <template>
