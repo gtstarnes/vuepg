0a80113645159a6094fac754f1cf69923d3c2b44
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-24 14:55:50 -0500

    reset

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,22 +1,9 @@
 <script setup lang="ts">
-    import {ref, computed} from 'vue';
 
-    const count = ref<number>(0);
-    const square = computed(() => {
-        return count.value * count.value
-    })
 </script>
 
 <template>
-    <section>
-        <div> {{ count }}</div>
-        <div>
-            <button @click="count++">+</button>
-            <button @click="count = 0">clr</button>
-            <button @click="count--">-</button>
-        </div>
-        <div>{{ square }}</div>
-    </section>
+
 </template>
 
 <style lang="css" scoped>
