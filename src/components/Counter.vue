623d7447b628893163778f6d48f5c4815d354f0a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-25 12:35:00 -0500

    reset

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,20 +1,9 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
 
-    const count = ref(0)
-    const square = computed(()=> {
-        return count.value * count.value
-    })
 </script>
 
 <template>
-    <span>{{ count }}</span>
-    <div>
-        <button @click="count++">+</button>
-        <button @click="count = 0">clr</button>
-        <button @click="count--">-</button>
-    </div>
-    <span>{{square}}</span>
+
 </template>
 
 <style lang="css" scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 
-
-  const name = ref('')
 </script>
 
 <template>
-  <input type="text" placeholder="enter name" v-model="name" />
-  <div v-if="name !== ''">
-    Hello, {{ name }}
-  </div>
-  <div v-else>
-    Please introduction yourself
-  </div>
+
 </template>
 
 <style scoped>
