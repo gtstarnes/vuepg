7f558c2fa750e334dd55ba0d17707116162457dd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-25 13:08:06 -0500

    add card to app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import Counter from './components/Counter.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
   <HelloWorld />
   <Counter />
+  <Card />
 </template>
 
 <style scoped>
