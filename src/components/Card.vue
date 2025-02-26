ef2e95f5f457f29ca11e6e5563eb835c787788c0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-02-26 10:46:18 -0500

    create cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,56 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+    const cards = ref([
+        {
+            id: 1,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+        {
+            id: 1,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+        {
+            id: 2,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+        {
+            id: 3,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+        {
+            id: 4,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+        {
+            id: 5,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+        {
+            id: 6,
+            name: 'sdaca sdc',
+            des: 'as cdsacd csafds fsa. dsacfdvcdsa . dsdaca sac',
+            price: '$17.32',
+        },
+    ])
 </script>
 
 <template>
-
+    <section>
+        
+    </section>
 </template>
 
 <style lang="css">
