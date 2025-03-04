3c32339c2717cf755b1b2c2251374089d60e5abb
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-04 13:14:45 -0500

    create logged In page

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+    import Signup from './signup.vue';
+import { loggedIn } from './store'
 </script>
 
 <template>
-
+    <section>
+        <div v-if="loggedIn.status">
+            Hello, {{ loggedIn.user }}
+            <button>Log Out</button>
+        </div>
+    </section>
 </template>
 
 <style lang="css">
