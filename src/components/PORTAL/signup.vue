dcbbed95b7492f63fe22de6537747d33770f0abc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-04 13:25:26 -0500

    filled in html

--- src/components/PORTAL/signup.vue
+++ src/components/PORTAL/signup.vue
@@ -3,13 +3,25 @@
 
 <template>
     <form>
-        <input />
-        <input />
-        <input />
         <div>
-            <button></button>
-            <button></button>
-            <button></button>
+            <label for="username">Username: </label>
+            <input name="username" id="username" 
+                placeholder="enter username" type="text" />
+        </div>
+        <div>
+            <label for="email">Email: </label>
+            <input name="email" id="email" 
+            placeholder="enter email" type="email" />
+        </div>
+        <div>
+            <label for="password">Password: </label>
+            <input name="password" id="password" 
+            placeholder="enter password" type="password" />
+        </div>
+        <div>
+            <button type="submit">Sign Up</button>
+            <button type="reset">Cancel</button>
+            <button type="button">Login</button>
         </div>
     </form>
 </template>
