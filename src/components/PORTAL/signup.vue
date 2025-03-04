8d72cfce84ff60341b6eb5f8fbdd154d2f62f76e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-04 13:39:19 -0500

    create initial sign up

--- src/components/PORTAL/signup.vue
+++ src/components/PORTAL/signup.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
-    import { toggle, loggedIn } from './store';
+    import { ref } from 'vue';
+import { toggle, loggedIn } from './store';
+
+    const username = ref('');
+    
+    function handleSubmit(e : Event) {
+        e.preventDefault();
+        loggedIn.user = username.value;
+        loggedIn.status = true;
+    }
 </script>
 
 <template>
-    <form>
+    <form @submit="handleSubmit">
         <div>
             <label for="username">Username: </label>
             <input name="username" id="username" 
-                placeholder="enter username" type="text" />
+                placeholder="enter username" type="text" v-model="username"/>
         </div>
         <div>
             <label for="email">Email: </label>
