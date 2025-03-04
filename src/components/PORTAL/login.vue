a69c5494b259d94a670cf4d1c57396c58519aec4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-04 13:43:35 -0500

    create html

--- src/components/PORTAL/login.vue
+++ src/components/PORTAL/login.vue
@@ -1,8 +1,28 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+
+    const username = ref('')
 </script>
 
 <template>
-    <div>login</div>
+    <form>
+        <div>
+            <label for="username">Username: </label>
+            <input name="username" id="username" 
+                placeholder="enter username" type="text" v-model="username"/>
+        </div>
+        <div>
+            <label for="password">Password: </label>
+            <input name="password" id="password" 
+            placeholder="enter password" type="password" />
+        </div>
+        <div>
+            <button type="submit">Login</button>
+            <button type="reset">Cancel</button>
+            <button type="button">Sign Up</button>
+        </div>
+    </form>
 </template>
 
 <style lang="css">
