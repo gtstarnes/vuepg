24ae12dc386ae9c699fc195d2c3f9824db9f3598
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-04 13:20:22 -0500

    added portal

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Counter from './components/Counter.vue'
 import Card from './components/Card.vue'
+import Portal from './components/PORTAL/portal.vue';
 </script>
 
 <template>
   <HelloWorld />
+  <Portal />
   <Counter />
   <Card />
 </template>
