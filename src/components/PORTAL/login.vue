52879869ab0ed3aafc074a2558ab5e178eb854c6
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-05 11:24:35 -0500

    fill in html

--- src/components/PORTAL/login.vue
+++ src/components/PORTAL/login.vue
@@ -5,17 +5,18 @@
 <template>
     <form>
         <div>
-            <label for=""></label>
-            <input id="" name="" placeholder="" type="" />
+            <label for="username">Username: </label>
+            <input id="username" name="username" 
+                placeholder="enter username" type="text" />
         </div>
         <div>
-            <label for=""></label>
-            <input id="" name="" placeholder="" type="" />
+            <label for="password">Password: </label>
+            <input id="password" name="password" placeholder="password" type="password" />
         </div>
         <div>
-            <button></button>
-            <button></button>
-            <button></button>
+            <button type="submit">Sign In</button>
+            <button type="reset">Cancel</button>
+            <button type="button">Sign Up</button>
         </div>
     </form>
 </template>
