c6a95f02edccbec6b9cfc2a493a3ae5d719c6437
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-05 11:35:27 -0500

    create logout function

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -2,13 +2,19 @@
     import { login, toggle} from './store.ts'
     import Login from './Login.vue';
     import Signup from './Signup.vue';
+
+    function logOut () {
+        login.user = ''
+        login.status = false
+        toggle.state = false
+    }
 </script>
 
 <template>
     <section>
         <div v-if="login.status === true">
             <span>Hello, {{ login.user }}</span>
-            <button>Log Out</button>
+            <button @click="logOut">Log Out</button>
         </div>
         <div v-else-if="toggle.state === false">
             <Login />
