c506410275ca41e2ce723f9e72eb69182eef265d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-05 11:22:51 -0500

    create html

--- src/components/PORTAL/login.vue
+++ src/components/PORTAL/login.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
-    <p>test login</p>
+    <form>
+        <div>
+            <label for=""></label>
+            <input id="" name="" placeholder="" type="" />
+        </div>
+        <div>
+            <label for=""></label>
+            <input id="" name="" placeholder="" type="" />
+        </div>
+        <div>
+            <button></button>
+            <button></button>
+            <button></button>
+        </div>
+    </form>
 </template>
 
 <style lang="css">
