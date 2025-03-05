9ccd0391b2a93d603504daf7d0178b1f80552c60
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-05 11:18:30 -0500

    create basic html

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
-
+    <section>
+        <div v-if="login.status === true"></div>
+        <div v-else-if="toggle.state === false"></div> 
+        <div v-else></div>
+    </section>
 </template>
 
 <style lang="css">
