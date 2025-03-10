dd2ee51972c5d39e1d3f6501dadb46fe751ef13e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-10 12:50:27 -0400

    create routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,4 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import HelloWorld from "../components/HelloWorld.vue";
 import Card from "../components/Card.vue";
 import Counter from "../components/Counter.vue";
-import portal from "../components/PORTAL/portal.vue";
+import Portal from "../components/PORTAL/portal.vue";
+
+const routes = [
+    {path: '/hello', component: HelloWorld},
+    {path: '/cards?', component: Card},
+    {path: '/counter', component: Counter},
+    {path: '/portal', component: Portal},
+]
