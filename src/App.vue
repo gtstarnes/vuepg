20e714f165180c2c879419c3073dd42056b5b0b7
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-10 12:54:54 -0400

    removed imports

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import Counter from './components/Counter.vue'
-import Card from './components/Card.vue'
-import Portal from './components/PORTAL/portal.vue';
+
 </script>
 
 <template>
