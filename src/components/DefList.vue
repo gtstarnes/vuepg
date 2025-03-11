4f60dcd6de1c0e7e4b748acfcf71478608afd713
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-11 13:05:06 -0400

    reset

--- src/components/DefList.vue
+++ src/components/DefList.vue
@@ -2,25 +2,9 @@
 </script>
 
 <template>
-    <dl>
-        <dt>Hola</dt>
-        <dd>hello</dd>
-        <dt>Salve</dt>
-        <dd>hello</dd>
-        <dt>Hei</dt>
-        <dd>hello</dd>
-    </dl>
+
 </template>
 
 <style lang="css" scoped>
-    dl {
-        display: grid;
-        grid-template-columns: auto 1fr;
-    }
-    dt {
-        font-weight: bold;
-    }
-    dt::after {
-        content: ":";
-    }
+
 </style>
