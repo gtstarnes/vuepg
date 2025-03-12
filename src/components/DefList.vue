ac1b3320674616e31d10b354620305b86b3311b4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-12 12:44:26 -0400

    create words

--- src/components/DefList.vue
+++ src/components/DefList.vue
@@ -4,6 +4,12 @@ import { ref } from 'vue';
 
     const steps = ref(['step 1', 'step 2', 'step 3'])
     const things = ref(['thing', 'thing', 'thing'])
+    const words = ref({
+        'Hola': 'hello',
+        'Vale': 'bye',
+        'Entschuldigung': 'excuse me',
+        'Sezione': 'section',
+    })
 </script>
 
 <template>
