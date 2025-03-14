594cdc3a3baec0b0c4951dcd9451470ff52c8628
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-14 12:54:19 -0400

    add styles

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -14,4 +14,17 @@
     </section>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    section {
+        display: flex;
+        justify-content: center;
+        width: 100vw;
+        height: 90vh;
+
+        ul {
+            list-style: none;
+            display: grid;
+            grid-template-columns: 1fr 1fr 1fr;
+        }
+    }
+</style>
