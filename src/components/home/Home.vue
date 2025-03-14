bcdd4302bbea6344c3486999a965a04e3d550386
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-14 12:37:51 -0400

    set up ul list

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<script setup lang="ts">
+    import { routes } from '../../router';
+    import HomeCard from './HomeCard.vue';
+</script>
+<template>
+    <section>
+        <nav>
+            <ul>
+                <li v-for="({path}) in routes" :key="path">
+                    <HomeCard />
+                </li>
+            </ul>
+        </nav>
+    </section>
+</template>
 
 <style lang="css" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,10 @@ import Card from "../components/Card.vue";
 import Counter from "../components/Counter.vue";
 import Portal from "../components/PORTAL/portal.vue";
 import DefList from "../components/DefList.vue";
+import Home from "../components/home/Home.vue";
 
 const routes = [
+    {path: '/', component: Home},
     {path: '/hello', component: HelloWorld},
     {path: '/card', component: Card},
     {path: '/cards', component: Card},
