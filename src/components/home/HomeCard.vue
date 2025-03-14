590ee9f9277e21b51f9be75faa4d25490dc9620c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-14 12:42:52 -0400

    create props

--- src/components/home/HomeCard.vue
+++ src/components/home/HomeCard.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    const props = defineProps<{
+        route: string,
+        name: string
+    }>()
+</script>
 
-<template></template>
+<template>
+    <article>
+        
+    </article>
+</template>
 
 <style lang="css" scoped></style>
