b607d9342b67ce51796c238531a27b53f5f14ab9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-14 13:03:24 -0400

    add styles

--- src/components/home/HomeCard.vue
+++ src/components/home/HomeCard.vue
@@ -6,9 +6,30 @@
 </script>
 
 <template>
-    <article>
-        <RouterLink to="{{props.route}}">{{ props.name }}</RouterLink>
-    </article>
+    <button>
+        <RouterLink to="{{props.route}}">
+            {{ props.name }}
+        </RouterLink>
+    </button>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    button {
+        width: 12rem;
+        height: 16rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-shadow: 8px 8px 10px rgba(0,0,0,0.2);
+        cursor: pointer;
+    }
+
+    button:hover {
+        border: 0.25rem solid;
+    }
+    a {
+        text-decoration: none;
+        font-size: 2rem;
+        color: black;
+    }
+</style>
