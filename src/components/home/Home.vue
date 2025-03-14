c2ab00de6314f480a34b85a21e60e4e762dbdc8f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-14 13:10:21 -0400

    added gap to grid

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -20,11 +20,13 @@
         justify-content: center;
         width: 100vw;
         height: 90vh;
+        
 
         ul {
             list-style: none;
             display: grid;
             grid-template-columns: 1fr 1fr 1fr;
+            gap: 10px;
         }
     }
 </style>
