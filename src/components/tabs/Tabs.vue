601bc3459ce1fa06a5833d6cdd2ad076ac9bc13a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-18 17:36:46 -0400

    create tabs

---
+++ src/components/tabs/Tabs.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { users } from './userInfo';
+
+
+const user = ref(users)
+const tabs = ref(["Posts", "Reels", "Tagged"])
+const active = ref("Posts")
+</script>
+
+<template>
+    <section>
+        <div>
+            <button v-for="tab in tabs" :key="tab">{{ tab }}</button>
+        </div>
+        <section>
+            <div v-if="active === 'Posts'">Posts</div>
+            <div v-else-if="active === 'Reels'">Reels</div>
+            <div v-else-if="active === 'Tagged'">Tagged</div>
+        </section>
+    </section>
+</template>
+
+<style lang="css" scoped>
+</style>
