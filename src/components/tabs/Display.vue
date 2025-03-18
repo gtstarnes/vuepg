e55664cf506aa762935882de2cb797b910db7d29
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-18 17:39:34 -0400

    create simple display

--- src/components/tabs/Display.vue
+++ src/components/tabs/Display.vue
@@ -1,5 +1,11 @@
 <script lang="ts"></script>
 
-<template></template>
+<template>
+    <section>
+        <div v-if="active === 'Posts'">Posts</div>
+        <div v-else-if="active === 'Reels'">Reels</div>
+        <div v-else-if="active === 'Tagged'">Tagged</div>
+    </section>
+</template>
 
 <style lang="css"></style>
