6a22a8254dae370c02e6ee814ab24661d9ddb574
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-18 17:24:26 -0400

    add reels and tagged

--- src/components/tabs/userInfo.ts
+++ src/components/tabs/userInfo.ts
@@ -23,6 +23,47 @@ export const users = [
                 likes: 43,
                 comments: 32,
             },
-        ]
-    }
+        ],
+        reels: [
+            {
+                id: 4,
+                img: 'black',
+                likes: 43,
+                comments: 32,
+            },
+            {
+                id: 5,
+                img: 'green',
+                likes: 43,
+                comments: 32,
+            },
+            {
+                id: 6,
+                img: 'blue',
+                likes: 43,
+                comments: 32,
+            },
+        ],
+        tagged: [
+            {
+                id: 43,
+                img: 'olivegreen',
+                likes: 43,
+                comments: 32,
+            },
+            {
+                id: 523,
+                img: 'red',
+                likes: 43,
+                comments: 32,
+            },
+            {
+                id: 61,
+                img: 'gold',
+                likes: 43,
+                comments: 32,
+            },
+        ],
+        
+    },
 ]
