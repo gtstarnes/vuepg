f86bccdb4a28dd967e11be58f106f3d56c2d1b2c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-18 17:21:31 -0400

    create users

---
+++ src/components/tabs/userInfo.ts
@@ -0,0 +1,28 @@
+export const users = [
+    {
+        name: '',
+        username: '',
+        followers: 0,
+        following: 0,
+        posts: [
+            {
+                id: 1,
+                img: 'blue',
+                likes: 43,
+                comments: 32,
+            },
+            {
+                id: 2,
+                img: 'pink',
+                likes: 43,
+                comments: 32,
+            },
+            {
+                id: 3,
+                img: 'blueviolet',
+                likes: 43,
+                comments: 32,
+            },
+        ]
+    }
+]
