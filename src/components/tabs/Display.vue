c19cdce4ef455f138b07f327faf43d98eaca722d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-18 17:46:48 -0400

    imported users

--- src/components/tabs/Display.vue
+++ src/components/tabs/Display.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { users } from './userInfo';
+
     const props = defineProps({
         active: String,
     })
+    const user = ref(users)
 </script>
 
 <template>
     <section>
-        <div v-if="active === 'Posts'">Posts</div>
+        <div v-if="active === 'Posts'">
+            
+        </div>
         <div v-else-if="active === 'Reels'">Reels</div>
         <div v-else-if="active === 'Tagged'">Tagged</div>
     </section>
