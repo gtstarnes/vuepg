283889fd82a8cf773d3eb60c7f2e9df78a87b7f0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-20 13:48:53 -0400

    add some styles

--- src/components/meter/Meter.vue
+++ src/components/meter/Meter.vue
@@ -12,12 +12,30 @@ import { computed, ref } from 'vue';
     <div>
         <span class="option">
             <meter  max="1" value="0.5"></meter>
-            <button type="button"></button> 
+            <button type="button">Vote for A</button> 
         </span>
+        <span class="option">
             <meter  max='1' min="0" value="0.5"></meter>
+            <button type="button">Vote for B</button> 
+        </span>
 
     </div>
 </template>
 
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    div {
+        span {
+            display: flex;
+            flex-direction: column;
+
+            button, meter {
+                width: 50%;
+            }
+
+            meter {
+                height: 5rem;
+            }
+        }
+    }
+</style>
