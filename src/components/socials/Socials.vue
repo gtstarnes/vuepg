d1488d933c43c934cf5f9302666b756ec8a2f46a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-27 13:01:08 -0400

    add html

--- src/components/socials/Socials.vue
+++ src/components/socials/Socials.vue
@@ -1,7 +1,20 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import {getUser} from './users'
+
+
+    const user = ref(getUser(0))
+    const {pic, name, city, country, bio, links} = user.value
 </script>
 
-<template></template>
+<template>
+    <div class="container">
+        <div class="pic" :style="{backgroundColor: pic}"></div>
+        <h3>{{name}}</h3>
+        <div class="location">{{ city }}, {{ country }}</div>
+        <p>{{ bio }}</p>
+    </div>
+</template>
 
 <style lang="css" scoped>
 </style>
