8a903c7512719626c228fd7c330e8b34d37beb03
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-27 13:22:21 -0400

    add styles

--- src/components/socials/Socials.vue
+++ src/components/socials/Socials.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import {getUser} from './users'
+import SocialLink from './SocialLink.vue';
 
 
     const user = ref(getUser(0))
@@ -9,12 +10,57 @@ import {getUser} from './users'
 
 <template>
     <div class="container">
-        <div class="pic" :style="{backgroundColor: pic}"></div>
-        <h3>{{name}}</h3>
-        <div class="location">{{ city }}, {{ country }}</div>
-        <p>{{ bio }}</p>
+        <article>
+            <div class="pic" :style="{backgroundColor: pic}"></div>
+            <h3>{{name}}</h3>
+            <div class="location">{{ city }}, {{ country }}</div>
+            <p>"{{ bio }}"</p>
+            <ul>
+                <li v-for="{title, href} in links" :key="title">
+                    <SocialLink :title :href />
+                </li>
+            </ul>
+        </article>
     </div>
 </template>
 
 <style lang="css" scoped>
+    .container {
+        background-color: black;
+        height: 95vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+
+        article {
+            display: flex;
+            flex-direction: column;
+            width: 16rem;
+            height: auto;
+            background-color: rgb(49, 48, 48);
+            border-radius: 20px;
+            padding: 2rem;
+            align-items: center;
+
+            .pic {
+                height: 4rem;
+                width: 4rem;
+                border-radius: 50px;
+            }
+
+            .location {
+                color: greenyellow
+            }
+
+            ul {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+
+        }
+
+    }
 </style>
