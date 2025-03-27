449bd097dcfaeefe5465b18b49b498efa42659b4
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-03-27 13:27:01 -0400

    add styles

--- src/components/socials/SocialLink.vue
+++ src/components/socials/SocialLink.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
-    <a :href="href"> {{ title }}</a>
+    <button><a :href="href"> {{ title }}</a></button>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    a {
+        text-decoration: none;
+        color: white;
+        font-weight: bold;
+        width: 100%;
+    }
+    button {
+        background-color: rgb(80, 80, 80);
+        height: 3rem;
+        border: none;
+        border-radius:  8px;
+        width: 15rem;
+    }
+</style>
