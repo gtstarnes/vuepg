72153bce172eaf076b037e0f82a02b812f67129a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-01 16:30:04 -0400

    add img

--- src/components/product/productInfo.ts
+++ src/components/product/productInfo.ts
@@ -5,6 +5,7 @@ const products = [
         des: 'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
         price: "$149.99",
         ogPrice: "$1969.99",
+        img: 'grey',
     },
 ]
 
