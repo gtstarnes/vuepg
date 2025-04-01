e8c1fec764cfd7c668213585502ea493aede289f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-01 16:24:17 -0400

    create products array

---
+++ src/components/product/productInfo.ts
@@ -0,0 +1,13 @@
+const products = [
+    {
+        category: 'perfume',
+        name: 'Gabrielle Essence Eau Dr Parfum',
+        des: 'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
+        price: "$149.99",
+        ogPrice: "$1969.99",
+    },
+]
+
+
+
+export {products}
