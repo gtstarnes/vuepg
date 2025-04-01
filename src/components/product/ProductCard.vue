c1875cc5ccc269a73bfaa1ed2c477ea1ac66e761
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-01 16:27:41 -0400

    create html

--- src/components/product/ProductCard.vue
+++ src/components/product/ProductCard.vue
@@ -1,7 +1,22 @@
 <script lang="ts" setup>
 </script>
 
-<template></template>
+<template>
+    <section>
+        <article>
+            <div></div>
+            <div>
+                <h5></h5>
+                <h1></h1>
+                <p></p>
+                <div>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </article>
+    </section>
+</template>
 
 <style lang="css" scoped>
 </style>
