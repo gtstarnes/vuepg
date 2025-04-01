916cf57ea758edf3313eac60b24df9f1f0cd9e79
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-01 16:49:24 -0400

    styles

--- src/components/product/ProductButton.vue
+++ src/components/product/ProductButton.vue
@@ -6,4 +6,15 @@
     </button>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    button {
+        background-color: green;
+        border: none;
+        border-radius: 6px;
+        height: 3.2rem;
+        width: 100%;
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+    }
+</style>
