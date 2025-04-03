2ce64328c0729d5b5e7f75a6cee674dfa348d7cc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 14:07:12 -0400

    create project info

--- src/components/ecommerce/ecomInfo.ts
+++ src/components/ecommerce/ecomInfo.ts
@@ -2,3 +2,11 @@ const user = {
     username: 'BobDucca',
     profilePic: 'purple',
 }
+
+const productInfo = {
+    company: '',
+    name: '',
+    des: '',
+    price: 125.00,
+    discount: 0.5,
+}
