146bfe341512f062e7dd014e2a7a0c03c8692587
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 15:15:28 -0400

    reset styles

--- src/components/ecommerce/EcomPics.vue
+++ src/components/ecommerce/EcomPics.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { productInfo } from './ecomInfo';
 
 
     const pics = productInfo.pics
     const active = ref(0)
-
+    const activePic = computed(() => {
+        return pics[active.value]
+    })
     function changeActive(index:number){
         if (active.value !== index) {
             active.value
@@ -15,7 +17,7 @@ import { productInfo } from './ecomInfo';
 
 <template>
     <section>
-        <div class="main" :style="{backgroundColor: pics[active]}"></div>
+        <div class="main" :style="{backgroundColor: activePic}"></div>
         <div class="row">
             <div class="other" :class="index===active ? 'active': ''"
              v-for="(pic, index) in pics" :key="index"
@@ -26,33 +28,6 @@ import { productInfo } from './ecomInfo';
 </template>
 
 <style lang="css" scoped>
-    section {
-        display: flex;
-        flex-direction: column;
-        gap: 1rem;
-        align-items: center;
-        justify-content: center;
-        .main {
-            height: 20rem;
-            width: 20rem;
-            border-radius: 10px;
-        }
-        .row {
-            display: flex;
-            gap: 1rem;
 
-            .other {
-                height: 4rem;
-                width: 4rem;
-                border-radius: 4px;
-                cursor: pointer;
-            }
-        }
-        .active {
-            opacity: 0.5;
-            border: 2px solid orange
-        }
-
-    }
 
 </style>
