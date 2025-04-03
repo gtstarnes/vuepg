7ba4355de20ace0a46a3083bc2f050cb0e01bf31
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 14:50:57 -0400

    add svg / styles

--- src/components/ecommerce/EcomButton.vue
+++ src/components/ecommerce/EcomButton.vue
@@ -1,17 +1,27 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import EcomCart from './EcomCart.vue';
+
+</script>
 
 <template>
-    <button>Add to Cart</button>
+    <button>
+        <EcomCart />
+        Add to Cart
+    </button>
 </template>
 
 <style lang="css" scoped>
     button {
         background-color: orange;
         color: black;
+        font-weight: bold;
         border: none;
         border-radius: 4px;
         cursor: pointer;
         height: 2rem;
         width: 12rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 </style>
