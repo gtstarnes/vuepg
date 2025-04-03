ad0af50e8291aa4c19d426d02ca656f3f5e89564
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 13:49:04 -0400

    import components

--- src/components/ecommerce/Ecom.vue
+++ src/components/ecommerce/Ecom.vue
@@ -1,5 +1,16 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import EcomCard from './EcomCard.vue';
+import EcomNav from './EcomNav.vue';
+import EcomPics from './EcomPics.vue';
 
-<template></template>
+</script>
+
+<template>
+    <EcomNav />
+    <section>
+        <EcomPics />
+        <EcomCard />
+    </section>
+</template>
 
 <style lang="css" scoped></style>
