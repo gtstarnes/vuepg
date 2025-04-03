63fbc78606ec1191a8911abe1ad1bfb2fc1bc527
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 14:12:44 -0400

    add info

--- src/components/ecommerce/ecomInfo.ts
+++ src/components/ecommerce/ecomInfo.ts
@@ -4,9 +4,9 @@ const user = {
 }
 
 const productInfo = {
-    company: '',
-    name: '',
-    des: '',
+    company: 'Sneaker Company',
+    name: 'Fall Limited Edition Sneakers',
+    des: 'Thses low-profile sneakers are your perfect casual wear companion. Featuring a durable rudder outer sole, they\'ll withstand everything the weather can offer.',
     price: 125.00,
     discount: 0.5,
 }
