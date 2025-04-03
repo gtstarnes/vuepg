73309d86e0cb5e9ee11028186d37961167f83550
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 13:45:01 -0400

    create html scaffold

--- src/components/ecommerce/EcomCard.vue
+++ src/components/ecommerce/EcomCard.vue
@@ -1,5 +1,24 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import EcomButton from './EcomButton.vue';
+import EcomCount from './EcomCount.vue';
 
-<template></template>
+</script>
+
+<template>
+    <article>
+        <h6></h6>
+        <h1></h1>
+        <p></p>
+        <span>
+            <div></div>
+            <div></div>
+        </span>
+        <div></div>
+        <span>
+            <EcomCount />
+            <EcomButton />
+        </span>
+    </article>
+</template>
 
 <style lang="css" scoped></style>
