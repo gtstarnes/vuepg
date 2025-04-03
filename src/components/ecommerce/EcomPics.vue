7533fccce83865be60170deebb322f482fa5a197
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 15:05:04 -0400

    add styles

--- src/components/ecommerce/EcomPics.vue
+++ src/components/ecommerce/EcomPics.vue
@@ -8,10 +8,45 @@ import { productInfo } from './ecomInfo';
 </script>
 
 <template>
-    <div></div>
-    <div>
-        <div></div>
-    </div>
+    <section>
+        <div class="main" :style="{backgroundColor: pics[active]}"></div>
+        <div class="row">
+            <div class="other" :class="index===active ? 'active': ''"
+             v-for="(pic, index) in pics" :key="index"
+             :style="{backgroundColor: pic}"></div>
+        </div>
+    </section>
+
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    section {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        align-items: center;
+        justify-content: center;
+        .main {
+            height: 20rem;
+            width: 20rem;
+            border-radius: 10px;
+        }
+        .row {
+            display: flex;
+            gap: 1rem;
+
+            .other {
+                height: 4rem;
+                width: 4rem;
+                border-radius: 4px;
+                cursor: pointer;
+            }
+        }
+        .active {
+            opacity: 0.5;
+            border: 2px solid orange
+        }
+
+    }
+
+</style>
