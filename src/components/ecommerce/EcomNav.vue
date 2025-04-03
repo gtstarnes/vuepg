9ce735f6b17caab3afff2790777b806961388dff
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-04-03 14:29:19 -0400

    create html

--- src/components/ecommerce/EcomNav.vue
+++ src/components/ecommerce/EcomNav.vue
@@ -1,5 +1,19 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+    const links = ['Collections', 'Men', 'Women', 'About', 'Contact']
+</script>
 
-<template></template>
+<template>
+    <nav>
+        <span>
+            <h4>SNEAKERS</h4>
+            <ul>
+                <li v-for="link in links" :key="link">{{ link }}</li>
+            </ul>
+        </span>
+        <span>
+            <div class="profilePic"></div>
+        </span>
+    </nav>
+</template>
 
 <style lang="css" scoped></style>
