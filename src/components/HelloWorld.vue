6a88d0fd0554ca6febe6d01f9aad06a7c1b6ab12
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-15 14:06:09 -0400

    create v-model

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,12 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+let name = ref('')
+</script>
 
 <template>
-  <input type="text" placeholder="who are you,stranger??"/>
-  <h1></h1>
+  <input type="text" placeholder="who are you,stranger??" v-model="name"/>
+  <h1> Hello, {{ name || 'Stranger' }}</h1>
 </template>
 
 <style lang="css" scoped>
