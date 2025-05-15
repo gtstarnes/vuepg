b874b3e3f25742bd2bd61e9b476239bf0dde2b19
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-15 14:04:43 -0400

    create html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup></script>
 
-<template></template>
+<template>
+  <input type="text" placeholder="who are you,stranger??"/>
+  <h1></h1>
+</template>
 
 <style lang="css" scoped>
 </style>
