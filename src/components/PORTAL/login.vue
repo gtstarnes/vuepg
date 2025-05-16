2990d3cfad27ecaf4b3f5514692ab1f40604fbaf
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-16 15:42:19 -0400

    add html

--- src/components/PORTAL/login.vue
+++ src/components/PORTAL/login.vue
@@ -2,7 +2,21 @@
 </script>
 
 <template>
-
+    <form>
+        <div>
+            <label for="username">Username: </label>
+            <input name="username" id="username" type="text" placeholder="username" />
+        </div>
+        <div>
+            <label for="password">Password: </label>
+            <input name="password" id="password" type="text" placeholder="password" />
+        </div>
+        <div>
+            <button type="submit">Log In</button>
+            <button type="reset">Cancel</button>
+            <button type="button">Sign Up</button>
+        </div>
+    </form>
 </template>
 
 
