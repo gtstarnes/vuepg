e79a710251da7c1c298191a7ce5fc22d7424c823
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-23 11:58:32 -0400

    conditional rendering

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
+import Login from './Login.vue';
+import { loggedIn, signUp } from './store';
+
 
 </script>
 
 <template>
     <section>
-
+        <template v-if="loggedIn === false">
+            <template v-if="signUp === false">
+                <Login />
+            </template>
+            <template v-else>
+                <SignUp />
+            </template>
+        </template>
     </section>
 </template>
 
