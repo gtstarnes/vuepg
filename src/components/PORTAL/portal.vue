7b74e2d2504c260c875888840444b28a9e2fed6c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-23 11:30:25 -0400

    reset

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -1,27 +1,10 @@
 <script setup lang="ts">
-    import { login, toggle} from './store.ts'
-    import Login from './Login.vue';
-    import Signup from './Signup.vue';
 
-    function logOut () {
-        login.user = ''
-        login.status = false
-        toggle.state = false
-    }
 </script>
 
 <template>
     <section>
-        <div v-if="login.status === true">
-            <span>Hello, {{ login.user }}</span>
-            <button @click="logOut">Log Out</button>
-        </div>
-        <div v-else-if="toggle.state === false">
-            <Login />
-        </div> 
-        <div v-else>
-            <Signup />
-        </div>
+
     </section>
 </template>
 
