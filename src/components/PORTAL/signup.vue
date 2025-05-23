3180d9a1896d3bcfbba9bbfc3eb9e3f7289c6647
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-23 12:05:40 -0400

    reset

--- src/components/PORTAL/signup.vue
+++ src/components/PORTAL/signup.vue
@@ -1,27 +1,9 @@
 <script setup lang="ts">
-    import { ref } from 'vue';
-import { login, toggle, toggleState, handleSubmit } from './store';
 
-    const username = ref('')
 </script>
 
 <template>
-    <form @submit="(e) => handleSubmit(e, username)">
-        <div>
-            <label for="username">Username: </label>
-            <input id="username" name="username" 
-                placeholder="enter username" type="text" v-model="username" />
-        </div>
-        <div>
-            <label for="password">Password: </label>
-            <input id="password" name="password" placeholder="password" type="password" />
-        </div>
-        <div>
-            <button type="submit">Sign Up</button>
-            <button type="reset">Cancel</button>
-            <button type="button" @click="toggleState">Sign In</button>
-        </div>
-    </form>
+
 </template>
 
 <style lang="css">
