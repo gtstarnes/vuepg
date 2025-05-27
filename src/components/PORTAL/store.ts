f37cb7c3f6185b522272227500a2017ec00a6dc0
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-27 14:35:37 -0400

    create variables

--- src/components/PORTAL/store.ts
+++ src/components/PORTAL/store.ts
@@ -1,4 +1,13 @@
+import { ref } from "vue"
+
 type Login = {
     user: string,
     pass: string,
+    status: boolean,
 }
+
+let details = ref({
+    user: '',
+    pass: '',
+})
+let loginStatus = ref(false);
