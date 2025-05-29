1e7924884708540b5f86d74a0398c0905f846dc5
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-05-29 14:00:18 -0400

    create logged in conditional

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -1,7 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { getStatus } from './store';
+
+</script>
 
 
 <template>
+    <template v-if="getStatus().value === true">
+        <div>Logged In</div>
+    </template>
+    <template v-else>
+        
+    </template>
 </template>
 
 <style lang="css">
