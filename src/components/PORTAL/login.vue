8ccf002eef06b44a63973a24952d99e4faf97f8f
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-05 11:20:11 -0400

    add styles

--- src/components/PORTAL/login.vue
+++ src/components/PORTAL/login.vue
@@ -11,15 +11,15 @@ const inputs = ref({
 
 <template>
     <form>
-        <div>
+        <div class="inputs">
             <label for="username">Username: </label>
             <input name="username" id="username" placeholder="username" v-model="inputs.username" />
         </div>
-        <div>
+        <div class="inputs">
             <label for="password">Password: </label>
             <input name="password" id="password" placeholder="password" v-model="inputs.password" />
         </div>
-        <div>
+        <div class="controls">
             <button type="submit">Login</button>
             <button type="reset">Cancel</button>
         </div>
@@ -28,4 +28,31 @@ const inputs = ref({
 </template>
 
 <style lang="css">
+    form {
+        max-width: 16rem;
+        .inputs {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 0.25rem;
+        }
+        .controls {
+            margin-top: 0.5rem;
+            display: flex;
+            gap: .1rem;
+
+            button {
+                cursor: pointer;
+            }
+        }
+        p {
+            button {
+                border: none;
+                background-color: inherit;
+                color: blue;
+                text-decoration: underline;
+                cursor: pointer;
+            }
+        }
+    }
 </style>
