50d50b4a059dd94b3a601c65b5926bbb9e0dab45
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-17 10:27:33 -0400

    create basic html

--- src/components/tabs/Head.vue
+++ src/components/tabs/Head.vue
@@ -3,7 +3,19 @@
 </script>
 
 <template>
-    
+    <div class="profPic"></div>
+    <div>
+        <div>
+            <button>Follow</button>
+            <button>Message</button>
+        </div>
+        <div>
+            <span>post</span>
+            <span>followers</span>
+            <span>following</span>
+        </div>
+        <div></div>
+    </div>
 </template>
 
 <style lang="css" scoped>
