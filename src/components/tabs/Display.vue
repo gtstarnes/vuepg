9cd310725fc82f77d4311e7f4c8b78ead2274f31
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-17 11:17:00 -0400

    create display for reels and tagged

--- src/components/tabs/Display.vue
+++ src/components/tabs/Display.vue
@@ -6,22 +6,27 @@ import { changeTab, getActiveTab, getTabs, getUser } from './userInfo';
     const tabs = ref(getTabs())
     const active = ref(getActiveTab())
     const posts = ref(user.value.posts)
+    const reels = ref(user.value.reels)
+    const tagged = ref(user.value.tagged)
 </script>
 
 <template>
-    <h1>Hello</h1>
-    <div>
-        <button v-for="tab in tabs" @click="changeTab(tab)">{{ tab }}</button>
-    </div>
-    <section>
+    <section>    
+        <div>
+            <button v-for="tab in tabs" @click="changeTab(tab)">{{ tab }}</button>
+        </div>
         <template v-if="active === 'Posts'">
             <button v-for="post in posts"></button>
         </template>
-        
+        <template v-else-if="active === 'Reels'">
+            <button v-for="reel in reels"></button>
+        </template>
+        <template v-else-if="active === 'Tagged'">
+            <button v-for="t in tagged"></button>
+        </template>  
     </section>
-    
 </template>
 
 <style lang="css" scope>
-
+    
 </style>
