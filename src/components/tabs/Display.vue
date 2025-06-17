51064bb74a640d0a99de3584a61828072dc5172e
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-17 11:13:37 -0400

    create display for posts and change tab

--- src/components/tabs/Display.vue
+++ src/components/tabs/Display.vue
@@ -1,9 +1,25 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+import { changeTab, getActiveTab, getTabs, getUser } from './userInfo';
 
+    const user = ref(getUser())
+    const tabs = ref(getTabs())
+    const active = ref(getActiveTab())
+    const posts = ref(user.value.posts)
 </script>
 
 <template>
     <h1>Hello</h1>
+    <div>
+        <button v-for="tab in tabs" @click="changeTab(tab)">{{ tab }}</button>
+    </div>
+    <section>
+        <template v-if="active === 'Posts'">
+            <button v-for="post in posts"></button>
+        </template>
+        
+    </section>
+    
 </template>
 
 <style lang="css" scope>
