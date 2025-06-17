ab152b100bc4125c079399c452855572f421d3db
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-17 10:16:39 -0400

    reset

--- src/components/tabs/Display.vue
+++ src/components/tabs/Display.vue
@@ -1,46 +1,11 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { users } from './userInfo';
 
-    const props = defineProps({
-        active: String,
-    })
-    const user = ref(users)
 </script>
 
 <template>
-    <ul v-if="active === 'Posts'">
-        <li v-for="item in user[0].posts" :key="item.id">
-            <button :style="{backgroundColor: item.img}"></button>
-        </li>
-    </ul>
-    <ul v-else-if="active === 'Reels'">
-        <li v-for="item in user[0].reels" :key="item.id">
-            <button :style="{backgroundColor: item.img}"></button>
-        </li>
-    </ul>
-    <ul v-else-if="active === 'Tagged'">
-        <li v-for="item in user[0].tagged" :key="item.id">
-            <button :style="{backgroundColor: item.img}"></button>
-        </li>
-    </ul>
+    <h1>Hello</h1>
 </template>
 
 <style lang="css" scope>
-    ul {
-        width: 100%;
-        display: grid;
-        grid-template-columns: 12rem 12rem 12rem;
-        gap:1rem;
-        list-style:none;
-        margin: 0;
-        margin-top: 1rem;
-        padding-left: 0;
-        justify-content: center;
-        button {
-            height: 16rem;
-            width: 12rem;
-            cursor: pointer;
-        }
-    }
+
 </style>
--- src/components/tabs/Tabs.vue
+++ src/components/tabs/Tabs.vue
@@ -1,48 +1,11 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { users } from './userInfo';
-import Display from './Display.vue';
 
-
-const user = ref(users)
-const tabs = ref(["Posts", "Reels", "Tagged"])
-const active = ref("Posts")
-
-function changeActive(tab:string) {
-    if (active.value !== tab) {
-        active.value = tab
-    }
-}
 </script>
 
 <template>
-    <section>
-        <div>
-            <button v-for="tab in tabs" :key="tab" @click="changeActive(tab)">{{ tab }}</button>
-        </div>
-        <Display :active />
-    </section>
+
 </template>
 
 <style lang="css" scoped>
-    section {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        div {
-            width: 100%;
-            height: 2rem;
-            box-shadow: 0 10px 10px rgba(0,0,0,0.1);
-            display: flex;
-            align-items: center;
-            justify-content: space-evenly;
-        }
 
-        button {
-            background-color: inherit;
-            border: none;
-            cursor: pointer;
-        }
-    }
 </style>
