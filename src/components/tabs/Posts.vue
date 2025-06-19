3f1734e08a68833a466dd070517ff883f8607f2d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-19 14:17:34 -0400

    create props

--- src/components/tabs/Posts.vue
+++ src/components/tabs/Posts.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
+import type { Post } from './userInfo';
 
+    const props = defineProps<Post[]>()
 </script>
 
 <template>
     <div class="display">
-        <button class="post" v-for="({img, likes, comments}) in reels" 
+        <button class="post" v-for="({img, likes, comments}) in props" 
             :style="{backgroundColor: img}">
             <div class="PostInfo">
                 <span>{{ likes }}</span>
