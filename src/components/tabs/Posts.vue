bd8927fa2839c688326d28edc6ef7c7f01ccab4a
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-06-19 14:10:36 -0400

    create html

--- src/components/tabs/Posts.vue
+++ src/components/tabs/Posts.vue
@@ -1,5 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
 
-<template></template>
+</script>
+
+<template>
+    <div class="display">
+        <button class="post" v-for="({img, likes, comments}) in reels" 
+            :style="{backgroundColor: img}">
+            <div class="PostInfo">
+                <span>{{ likes }}</span>
+                <span>{{ comments }}</span>
+            </div>
+        </button>
+    </div>
+</template>
 
 <style lang="css" scoped></style>
