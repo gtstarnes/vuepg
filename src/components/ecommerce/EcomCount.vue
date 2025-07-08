c89e0f1cefdab1971e6574805de36b43b0910a70
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-08 14:07:05 -0400

    add basic html / css

--- src/components/ecommerce/EcomCount.vue
+++ src/components/ecommerce/EcomCount.vue
@@ -3,9 +3,30 @@
 </script>
 
 <template>
-
+    <div class="container">
+        <button>-</button>
+        <span>0</span>
+        <button>+</button>
+    </div>
 </template>
 
 <style lang="css" scoped>
+    button {
+        border: none;
+        background-color: inherit;
+        color: orange;
+        font-weight: bold;
+        cursor: pointer;
+    }
 
+    .container {
+        background-color: lightgrey;
+        width: 5em;
+        height: 2em;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.75em;
+        border-radius: 4px;
+    }
 </style>
