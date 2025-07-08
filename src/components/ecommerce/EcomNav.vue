c968f45d346678be58ba691407e36353227032b9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-08 14:22:15 -0400

    create basic html

--- src/components/ecommerce/EcomNav.vue
+++ src/components/ecommerce/EcomNav.vue
@@ -1,9 +1,18 @@
 <script lang="ts" setup>
+import EcomCart from './EcomCart.vue';
+
 
 </script>
 
 <template>
-
+    <nav>
+        <h2>SNEAKERS</h2>
+        <ul></ul>
+        <span>
+            <EcomCart />
+            <div></div>
+        </span>
+    </nav>
 </template>
 
 <style lang="css" scoped>
