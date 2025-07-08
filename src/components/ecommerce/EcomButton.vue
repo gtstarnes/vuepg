cce2c95ce704aab7294d4ec1dd02e8f95d90caad
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-08 13:44:11 -0400

    create css

--- src/components/ecommerce/EcomButton.vue
+++ src/components/ecommerce/EcomButton.vue
@@ -13,5 +13,17 @@ import EcomCart from './EcomCart.vue';
 </template>
 
 <style lang="css" scoped>
-
+    button {
+        background-color: orange;
+        color: black;
+        border: none;
+        width: 14em;
+        height: 2.5em;
+        border-radius: 4px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        gap: 1.5em;
+    }
 </style>
