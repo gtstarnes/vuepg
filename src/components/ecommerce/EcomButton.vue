60b2de53d94db3a440ef31487b3b684b58ae6951
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-08 13:40:17 -0400

    create button html

--- src/components/ecommerce/EcomButton.vue
+++ src/components/ecommerce/EcomButton.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
+import EcomCart from './EcomCart.vue';
+
 
 
 </script>
 
 <template>
-
+    <button>
+        <EcomCart />
+        Add to Cart
+    </button>
 </template>
 
 <style lang="css" scoped>
