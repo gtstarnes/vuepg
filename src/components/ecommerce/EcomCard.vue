54ed3eb1a98a14f33a6277e3316456bbf7eb8e48
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-07-08 13:50:45 -0400

    create basic html

--- src/components/ecommerce/EcomCard.vue
+++ src/components/ecommerce/EcomCard.vue
@@ -1,9 +1,25 @@
 <script lang="ts" setup>
+import EcomButton from './EcomButton.vue';
+import EcomCount from './EcomCount.vue';
+
 
 </script>
 
 <template>
-
+    <article>
+        <h5>company</h5>
+        <h1>product</h1>
+        <p>des</p>
+        <div>
+            <div>price</div>
+            <div>discount</div>
+        </div>
+        <div>og price</div>
+        <div>
+            <EcomCount />
+            <EcomButton />
+        </div>
+    </article>
 </template>
 
 <style lang="css" scoped>
