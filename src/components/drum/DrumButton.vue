41be0abf81fef8f867b102f9b57ee10d13f816fe
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-09-18 11:47:43 -0400

    add styles for clicks

--- src/components/drum/DrumButton.vue
+++ src/components/drum/DrumButton.vue
@@ -1,16 +1,39 @@
 <script lang="tsx" setup>
 
     defineProps<{
-        key: string,
+        letter: string,
         text: string
     }>()
 </script>
 
 <template>
     <button>
-        {{ key }}
+        {{ letter }}
         <span>{{ text }}</span>
     </button>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    button {
+        height: 3rem;
+        width: 3rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        border: 3px black solid;
+        font-weight: bold;
+
+        span {
+            font-size: 0.5rem;
+            font-weight: lighter;
+        }
+    }
+
+    button:active {
+        transform: scale(125%);
+        border-color: orange;
+        box-shadow: 0px 0px 10px rgba(255, 165, 0, 75%);
+    }
+</style>
