16fb323ec13e955bf8bea03844fec249282f4d30
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-09-18 11:31:17 -0400

    create list of drums

--- src/components/drum/Drum.vue
+++ src/components/drum/Drum.vue
@@ -1,8 +1,19 @@
-<script lang="tsx" setup></script>
+<script lang="tsx" setup>
+    import { ref } from 'vue';
+import { drumInfo } from './drumInfo';
+import DrumButton from './DrumButton.vue';
+
+</script>
 
 <template>
     <section>
-
+        <div>
+            <ul>
+                <li v-for="d in drumInfo">
+                    <DrumButton :key="d.key" :text="d.text"  />
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
@@ -11,5 +22,6 @@
         background: linear-gradient(white, grey);
         height: 96vh;
         width: 99vw;
+        margin: -5px;
     }
 </style>
