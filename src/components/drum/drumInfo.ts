a87887bd0d65fc46c74ea6929de7d365ebb1de5c
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-09-18 11:08:59 -0400

    create initial drumInfo

---
+++ src/components/drum/drumInfo.ts
@@ -0,0 +1,47 @@
+const drumInfo = [
+    {
+        key: "A",
+        text: "CLAP",
+        sound: '',
+    },
+    {
+        key: "S",
+        text: "HIHAT",
+        sound: '',
+    },
+        {
+        key: "D",
+        text: "KICK",
+        sound: '',
+    },
+        {
+        key: "F",
+        text: "OPENHAT",
+        sound: '',
+    },
+        {
+        key: "G",
+        text: "BOOM",
+        sound: '',
+    },
+        {
+        key: "H",
+        text: "RIDE",
+        sound: '',
+    },
+        {
+        key: "J",
+        text: "SNARE",
+        sound: '',
+    },
+        {
+        key: "K",
+        text: "TOM",
+        sound: '',
+    },
+    {
+        key: "L",
+        text: "TINK",
+        sound: '',
+    },
+]
