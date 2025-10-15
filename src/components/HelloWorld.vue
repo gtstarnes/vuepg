f4d8e5a3cd3038a8476894a44b4dd9e5fcef0956
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-10-15 19:07:07 -0400

    tried

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,22 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 
+
+let name = ref("")
 </script>
 
 <template>
-
+  <p>
+    Hello, 
+    <span v-if="name">{{ name }}</span>
+    <span v-else>Stranger</span>
+  </p>
+  <input type="text" placeholder="What's your name?" v-model="name" />
 </template>
 
 <style lang="css" scoped>
-
+  span {
+    font-weight: bold;
+    font-size: 1.5rem;
+  }
 </style>
