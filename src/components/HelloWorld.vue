ca0fac59a788c4812525af394ee134588b74efdc
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-11-11 20:53:04 -0500

    reset

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,15 +3,9 @@
 </script>
 
 <template>
-  <p>Hello, <output id="result"></output></p>
-  <form oninput="result.value = name.value">
-    <input type="text" id="name" name="name" placeholder="who are you?" />
-  </form>
+
 </template>
 
 <style lang="css" scoped>
-  output {
-    font-weight: bold;
-    font-size: 1.5rem;
-  }
+
 </style>
