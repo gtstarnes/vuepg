cfbcdcda5131488b76256f0368f31291a504c93d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-11-11 20:57:09 -0500

    v-model hello

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 
+
+let name = ref("")
 </script>
 
 <template>
-
+  <input type="text" placeholder="who are you" v-model="name" />
+  <div>
+    Hello, 
+    <span v-if="name">{{ name}}</span>
+    <span v-else>Stranger</span>
+  </div>
 </template>
 
 <style lang="css" scoped>
