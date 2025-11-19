d73e5a7a12d00d3438c0e60180290eacd8d9ba73
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-11-19 13:27:43 -0500

    create dummy users

--- src/components/PORTAL/store.ts
+++ src/components/PORTAL/store.ts
@@ -1,6 +1,20 @@
+import { ref } from "vue"
 
 type User = {
     username: string,
     name: string,
     password: string,
 }
+
+const users = ref<User[]>([
+    {
+        username: 'azzclappa',
+        name: 'Jeff',
+        password: '1234'
+    },
+    {
+        username: 'urmom',
+        name: 'Bob',
+        password: '1234'
+    },
+])
