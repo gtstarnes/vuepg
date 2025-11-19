2b9cf0ae0ccd895b7a4c0747d77904e84075cf88
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2025-11-19 14:22:24 -0500

    create simple login

--- src/components/PORTAL/portal.vue
+++ src/components/PORTAL/portal.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
+import { getPortal } from './store';
+
+const portal = getPortal()
 
 </script>
 
 <template>
+    <form>
+        <div v-for="input in portal?.inputs">
+            <label :for="input.name">{{ input.label }}: </label>
+            <input :type="input.type" :placeholder="input.placeholder"  />
+        </div>
+        <div>
+            <button>Submit</button>
+            <button>Cancel</button>
+        </div>
+    </form>
 
 </template>
 
