e8cd6b24d67df08a7d56ec0fe670651659d14df9
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2026-01-30 15:56:20 -0500

    create basic html for login

--- src/components/PORTAL/login.vue
+++ src/components/PORTAL/login.vue
@@ -3,7 +3,32 @@
 </script>
 
 <template>
-
+    <form>
+        <label>Username:
+            <input 
+                type="text"
+                placeholder="username"
+                name="username" id="username"
+            />
+        </label>
+        <label>Password
+            <input 
+                type="text"
+                placeholder="password"
+                name="password" id="password"
+            />
+        </label>
+        <div>
+            <button>Submit</button>
+            <button>Cancel</button>
+            <div>
+                Don't have an account? Create one 
+                <button>
+                    here
+                </button>
+            </div>
+        </div>
+    </form>
 </template>
 
 <style lang="css" scoped>
